a8a71695d9f2baf7e43c723354bccbd3eb14e231
tien anh <giakhang22102016@gmail.com>
2019-09-26 08:31:51 +0700

    fiox

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import sampleData from '../assets/data.json';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ReaderJson';
+  User :any = sampleData;
+  listUser: User[] = [];
+  ngOnInit(){
+    console.log(this.User);
+    this.listUser = this.User;
+    for(let i= 0;i <this.listUser.length;i++){
+      console.log(this.listUser[i].id +" " + this.listUser[i].name);
+    }
+  }
+
+  
+}
+export class User{
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  phone: string;
+  website: string;
 }
